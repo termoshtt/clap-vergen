7390339c313e540fbd229e65a0b1febde6b55005
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-08-06 15:34:11 +0900

    Load git info from vergen

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone)]
+pub struct Git {
+    /// `VERGEN_GIT_BRANCH` e.g. `feature/fun`
+    pub branch: String,
+    /// `VERGEN_GIT_COMMIT_TIMESTAMP` e.g. `2021-02-24T20:55:21+00:00`
+    pub commit_timestamp: String,
+    /// `VERGEN_GIT_SEMVER` e.g. `5.0.0-2-gf49246c`
+    pub semver: String,
+    /// `VERGEN_GIT_SHA` e.g. `f49246ce334567bff9f950bfd0f3078184a2738a`
+    pub sha: String,
+}
+
+impl Default for Git {
+    fn default() -> Self {
+        Git {
+            branch: env!("VERGEN_GIT_BRANCH").to_string(),
+            commit_timestamp: env!("VERGEN_GIT_COMMIT_TIMESTAMP").to_string(),
+            semver: env!("VERGEN_GIT_SEMVER").to_string(),
+            sha: env!("VERGEN_GIT_SHA").to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct VergenInfo {
+    pub git: Git,
 }
 
 #[cfg(test)]
@@ -7,8 +31,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn load_env() {
+        let info = VergenInfo::default();
+        dbg!(info);
     }
 }
